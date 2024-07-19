16746d213873d144345474418d00f4ca91c05d7d
Jeremy Xu <jeremyxu24@gmail.com>
2024-07-19 12:24:58 -0700

    updated dark and light theme

--- websocket/spreadsheet_app/src/App.js
+++ websocket/spreadsheet_app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar.ts';
+import NavBar from './NavBar';
 import TanstackTable from './TanstackTable.tsx'; // Import the TanstackTable component
 import { createTheme, MantineProvider } from '@mantine/core';
+import Demo from './Demo';
 
 const theme = createTheme({
   /** Put your mantine theme override here */
@@ -15,6 +16,7 @@ function App() {
       <div className="App">
         <div style={{ width: '200px' }}>
           <NavBar />
+          <Demo />
         </div>
         <div style={{ padding: '20px' }}>
           <TanstackTable /> {/* Include the TanstackTable component */}
@@ -25,3 +27,4 @@ function App() {
 }
 
 export default App;
+
